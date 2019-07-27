280c06d22f26e16b6fb63a74f095719d3b0c67fa
SOHAG <SohagChowdhury321@gmail.com>
2019-07-27 21:12:28 +0600

    Redux Form inpurt

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,15 +2,20 @@ import React from 'react';
 import {Field,reduxForm}  from 'redux-form';
 
 class StreamCreate extends React.Component{
-    renderInput(formProps){
-            return <input {...formProps.input}/>;
+    renderInput({input,label}){
+            return (
+            <div className="field">
+                <label>{label}</label>
+                <input {...input}/>
+            </div>
+            )
     }
     render(){
         return(
             <div>
-                <form >
-                    <Field name="title" component={this.renderInput}/>
-                    <Field name="description" component={this.renderInput}/>
+                <form  className="ui form">
+                    <Field name="title" component={this.renderInput} label="Enter Title:"/>
+                    <Field name="description" component={this.renderInput} label="Enter Description:"/>
                 </form>
             </div>
         )
